f3c7b8d4bff00ce0ba6c5e806ab117b6817468c8
javaustin <austinchrisyoung1@gmail.com>
2026-09-16 15:07:20 -0700

    remove inventory open listener

--- src/main/java/com/carrotguy69/ssg/SpeedSG.java
+++ src/main/java/com/carrotguy69/ssg/SpeedSG.java
@@ -385,22 +385,6 @@ public final class SpeedSG extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onContainerOpen(InventoryOpenEvent e) {
-        Player p = (Player) e.getPlayer();
-
-        Game game = Game.getByPlayer(p);
-
-        if (game == null) {
-            return;
-        }
-
-        if (p.getGameMode() != GameMode.CREATIVE && (e.getInventory().getType() != InventoryType.PLAYER && e.getInventory().getType() != InventoryType.CHEST)) {
-             e.setCancelled(true);
-        }
-
-    }
-
-    @EventHandler
     public void onInteract(PlayerInteractEvent e) {
 
         Player p = e.getPlayer();
